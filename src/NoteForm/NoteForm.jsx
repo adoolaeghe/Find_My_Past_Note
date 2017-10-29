62d0a9ad3051e21612d358a5a6ed5caad97696a9
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2017-10-29 12:49:28 +0000

    Note form can handle user input I have used an onchange function that set the state the to the user target

--- src/NoteForm/NoteForm.jsx
+++ src/NoteForm/NoteForm.jsx
@@ -6,13 +6,21 @@ class NoteForm extends Component {
   constructor(props){
     super(props);
     this.state = {
+      newNoteContent: '',
     };
+    this.handleUserInput = this.handleUserInput.bind(this)
+  }
+
+  handleUserInput(e){
+     this.setState({
+      newNoteContent: e.target.value
+     })
   }
 
   render(){
     return(
       <div className='formWrapper'>
-        <input className="noteInput" type="text" placeholder="this.state.newNoteContent" />
+        <input className="noteInput" type="text" placeholder="Write a new note..." value={this.state.newNoteContent} onChange={this.handleUserInput}/>
         <input className="noteButton" type="button" value="Search" />
       </div>
     )
